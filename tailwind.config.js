d39ce4666f5d1a7eca917652c9e390562462cd96
akashgreninja <akashuhulekal@gmail.com>
2023-03-26 15:21:01 +0530

    part 1 done

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+      },
+      height: {
+        "3/4": "60px",
+        forgif: "30rem",
+        "footer-height": "16rem",
+      },
+      width: {
+        forgif: "70vw",
+      },
+      margin: {
+        "4/5": "13rem",
+        check: "20%",
+      },
+      backgroundColor: {
+        "chakranti-200": "rgb(15 15 15)",
+        "footer-400": "rgb(22 21  23)",
+      },
+      backgroundImage: {
+        "garvi-background": "url('./images/1120_Slice-Illos-2-copy.gif')",
+      },
+      fontSize: {
+        bigg: "5rem",
+        smoller: "2rem",
+        medium: "3rem",
+      },
+      fontFamily: {
+        sans: ["CustomFont", "Helvetica", "Arial", "sans-serif"],
+      },
+      keyframes: {
+        bounce: {
+          "0%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-10px)" },
+          "100%": { transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        bounce: "bounce 1s infinite",
+      },
+    },
+  },
+};
